c41cae85d32dc223deac4737497984ef52a8a351
asurpbs <asur.1357268@gmail.com>
2025-01-13 20:57:52 +0530

    fix some minor bugs

--- admin/login.php
+++ admin/login.php
@@ -3,11 +3,11 @@
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
 
-    include '/components/connect.php';
+    include '../components/connect.php';
 
     if(isset($_COOKIE['user_id'])){
         $user_id = $_COOKIE['user_id'];
-        header('Location:/admin/pages/index.php');
+        header('Location:./pages/index.php');
         exit();
     }
 ?>
@@ -17,25 +17,29 @@
         <title>Signin - Admin | Sola Chemicals</title>
         <?php include '../data/meta-admin.php'; ?>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
-       <link rel="stylesheet" href="/assets/css/login.css">
+       <link rel="stylesheet" href="./assets/css/login.css">
     </head>
     <body>
         <div class="container">
             <div class="header">
-                <a href=""><img src="/public/apple-touch-icon.png" alt="ExamGIS_logo" width="70px" height="70px"><span></span></a>
+                <a href=""><img src="/public/apple-touch-icon.png" alt="sola chemicals logo" width="70px" height="70px"><span></span></a>
                 <h1>Sign in to Sola Chemicals</h1>
             </div>
-            <form action="./components/login-post.php" method="POST" enctype="multipart/form-data">
+            <form action="./components/login-post.php" method="post" enctype="multipart/form-data" >
                 <div class="form-group">
                     <input type="email" name="email" id="email" required class="inputField" autocomplete="off">
-                    <input type="password" name="pass" id="pass" required class="inputField" autocomplete="off">
-                    <button type="submit" name="submitlogin" class="btn">Sign In</button>
+                    <label for="email">Email</label>
                 </div>
+                <div class="form-group">
+                    <input type="password" name="pass" id="password" required class="inputField" autocomplete="off">
+                    <label for="password">Password</label>
+                </div>
+                <button type="submit" class="submitButton" name="submitlogin">Log in</button>
             </form>
         </div>
         <footer class="footer-del">
             <span class="author">
-                © 2025 Sola Chemical Company. All rights reserved.
+                © 2025 Hash Coders. All rights reserved.
             </span>
         <footer>
         <?php include "../data/bootstrap.php" ?>
