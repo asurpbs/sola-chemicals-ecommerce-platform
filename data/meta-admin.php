2e18f41251fd3c7ca98289f90daaf59671e39d3b
asurpbs <asur.1357268@gmail.com>
2025-01-13 20:37:28 +0530

    added indentation for all lines

--- data/meta-admin.php
+++ data/meta-admin.php
@@ -1,21 +1,21 @@
-<meta charset="utf-8"/>
-<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-<meta name="title" content="Sola Chemical Company"/>
-<meta name="description" content="Administrator site of the Sola Chemical Company"/>
-<!--
-<meta name="robots" content="index, follow"/>
-<meta name="googlebot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1"/>
-<meta name="bingbot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1"/>
-<link rel="canonical" href="https://sola.mthlpbs.hackclub.app/"/>
--->
-<link rel="icon" type="image/png" href="/public/favicon-96x96.png" sizes="96x96"/>
-<link rel="icon" type="image/svg+xml" href="/public/favicon.svg"/>
-<link rel="shortcut icon" href="/public/favicon.ico"/>
-<link rel="apple-touch-icon" sizes="180x180" href="/public/apple-touch-icon.png"/>
-<link rel="manifest" href="/public/site.webmanifest"/>
-<link rel="preconnect" href="https://fonts.googleapis.com"/>
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-<link rel="canonical" href="https://sola.mthlpbs.hackclub.app/"/>
-<meta http-equiv="Content-Security-Policy" content="default-src 'self'; img-src 'self' data:; script-src 'self'; style-src 'self' 'unsafe-inline'; connect-src 'self';"/>
-<meta http-equiv="x-ua-compatible" content="ie=edge"/>
-<meta name="referrer" content="no-referrer-when-downgrade"/>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta name="title" content="Sola Chemical Company"/>
+    <meta name="description" content="Administrator site of the Sola Chemical Company"/>
+    <!--
+    <meta name="robots" content="index, follow"/>
+    <meta name="googlebot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1"/>
+    <meta name="bingbot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1"/>
+    <link rel="canonical" href="https://sola.mthlpbs.hackclub.app/"/>
+    -->
+    <link rel="icon" type="image/png" href="/public/favicon-96x96.png" sizes="96x96"/>
+    <link rel="icon" type="image/svg+xml" href="/public/favicon.svg"/>
+    <link rel="shortcut icon" href="/public/favicon.ico"/>
+    <link rel="apple-touch-icon" sizes="180x180" href="/public/apple-touch-icon.png"/>
+    <link rel="manifest" href="/public/site.webmanifest"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+    <link rel="canonical" href="https://sola.mthlpbs.hackclub.app/"/>
+    <meta http-equiv="Content-Security-Policy" content="default-src 'self'; img-src 'self' data:; script-src 'self'; style-src 'self' 'unsafe-inline'; connect-src 'self';"/>
+    <meta http-equiv="x-ua-compatible" content="ie=edge"/>
+    <meta name="referrer" content="no-referrer-when-downgrade"/>
