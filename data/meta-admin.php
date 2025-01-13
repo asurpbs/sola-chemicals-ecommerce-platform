48388eae6e195461eaad0e7a48577958f1ff5f74
asurpbs <asur.1357268@gmail.com>
2025-01-13 20:22:53 +0530

    for testing

--- data/meta-admin.php
+++ data/meta-admin.php
@@ -5,11 +5,11 @@
 <meta name="bingbot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1"/>
 <link rel="canonical" href="https://sola.mthlpbs.hackclub.app/"/>
 -->
-<link rel="icon" type="image/png" href="./public/favicon-96x96.png" sizes="96x96"/>
-<link rel="icon" type="image/svg+xml" href="./public/favicon.svg"/>
-<link rel="shortcut icon" href="./public/favicon.ico"/>
-<link rel="apple-touch-icon" sizes="180x180" href="./public/apple-touch-icon.png"/>
-<link rel="manifest" href="./public/site.webmanifest"/>
+<link rel="icon" type="image/png" href="/public/favicon-96x96.png" sizes="96x96"/>
+<link rel="icon" type="image/svg+xml" href="/public/favicon.svg"/>
+<link rel="shortcut icon" href="/public/favicon.ico"/>
+<link rel="apple-touch-icon" sizes="180x180" href="/public/apple-touch-icon.png"/>
+<link rel="manifest" href="/public/site.webmanifest"/>
 <link rel="preconnect" href="https://fonts.googleapis.com"/>
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
 <link rel="canonical" href="https://sola.mthlpbs.hackclub.app/"/>
