9d73655f7d8623aa766ec1ddf6deb74ee13c4917
asurpbs <asur.1357268@gmail.com>
2025-01-14 12:44:03 +0530

    fixed automatically triggering issue of alert box, when the page loads

--- admin/login.php
+++ admin/login.php
@@ -1,39 +1,35 @@
 <?php
+
     include '../components/connect.php';
-    
+
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
-
-    function handleLogin() {
-        global $conn;
-        if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submitlogin'])){
-            $email = $_POST['email'];
-            $email = filter_var($email, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-            $pass = $_POST['pass'];
-            $pass = filter_var($pass, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-    
-            $select_user = $conn->prepare("SELECT * FROM `user` WHERE email = ? LIMIT 1");
-            $select_user->execute([$email]);
-            $row = $select_user->fetch(PDO::FETCH_ASSOC);
-    
-            if($select_user->rowCount() > 0 && password_verify($pass, $row['password'])){
-                setcookie('user_id', $row['id'], time() + 60*60*24*30, '/');
-                header('location:../pages/index.php');
-                exit();
-            } else {
-                $message = "Email or password invalid";
-                echo "<script type='text/javascript'>alert('$message');</script>";
-            }
-        }
-    }
 
     if(isset($_COOKIE['user_id'])){
         $user_id = $_COOKIE['user_id'];
         header('Location:./pages/index.php');
         exit();
     }
+
+    if(isset($_POST['submitlogin']) && !empty($_POST['email']) && !empty($_POST['pass'])){
+        $email = $_POST['email'];
+        $email = filter_var($email, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $pass = $_POST['pass'];
+        $pass = filter_var($pass, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     
-    handleLogin();
+        $select_user = $conn->prepare("SELECT * FROM `user` WHERE email = ? LIMIT 1");
+        $select_user->execute([$email]);
+        $row = $select_user->fetch(PDO::FETCH_ASSOC);
+       
+        if($select_user->rowCount() > 0 && password_verify($pass, $row['password'])){
+            setcookie('user_id', $row['id'], time() + 60*60*24*30, '/');
+            header('location:../pages/index.php');
+            exit();
+        } else {
+            $message = "Email or password invalid";
+            echo "<script type='text/javascript'>alert('$message');</script>";
+        }
+    }
 ?>
 
 <!DOCTYPE html>
