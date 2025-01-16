c14b7a77348c906c50a229301299cbfbe689e0da
asurpbs <asur.1357268@gmail.com>
2025-01-16 12:41:09 +0530

    fix:Update manifest link paths for consistency across files

--- data/meta-admin.php
+++ data/meta-admin.php
@@ -13,7 +13,7 @@
     <link rel="icon" type="image/svg+xml" href="/public/favicon.svg"/>
     <link rel="shortcut icon" href="/public/favicon.ico"/>
     <link rel="apple-touch-icon" sizes="180x180" href="/public/apple-touch-icon.png"/>
-    <link rel="manifest" href="/public/site.webmanifest"/>
+    <link rel="manifest" href="./public/site.webmanifest"/>
     <link rel="preconnect" href="https://fonts.googleapis.com"/>
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
     <link rel="canonical" href="https://hashcoders.alwaysdata.net/admin/"/>
