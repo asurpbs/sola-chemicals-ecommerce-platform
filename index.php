652cbfb91983d6a7e1dee7c34cb221bcf45d354a
asurpbs <asur.1357268@gmail.com>
2025-01-16 09:17:00 +0530

    refactor:Remove navbar inclusion from index.php

--- index.php
+++ index.php
@@ -290,8 +290,6 @@
         </div>
       </div>
     </section>
-    <!-- navbar -->
-    <?php include './components/header.html'; ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="./assets/js/script.js"></script>
   </body>
