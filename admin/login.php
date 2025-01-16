edd30cbaee2e23e60f39acf878e2e336aab36789
asurpbs <asur.1357268@gmail.com>
2025-01-16 07:51:18 +0530

    insert meta code snippet instead of referencing

--- admin/login.php
+++ admin/login.php
@@ -34,7 +34,25 @@
 <html lang="en">
     <head>
         <title>Signin - Admin | Sola Chemicals</title>
-        <?php include '../data/meta-admin.php'; ?>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta name="title" content="Sola Chemical Company"/>
+        <meta name="description" content="Administrator site of the Sola Chemical Company"/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+        <!--
+        <meta name="robots" content="index, follow"/>
+        <meta name="googlebot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1"/>
+        <meta name="bingbot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1"/>
+        <link rel="canonical" href="https://hashcoders.alwaysdata.net/admin/"/>
+        -->
+        <link rel="icon" type="image/png" href="../public/favicon-96x96.png" sizes="96x96"/>
+        <link rel="icon" type="image/svg+xml" href="../public/favicon.svg"/>
+        <link rel="shortcut icon" href="../public/favicon.ico"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="/public/apple-touch-icon.png"/>
+        <link rel="manifest" href="../public/site.webmanifest"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link rel="canonical" href="https://hashcoders.alwaysdata.net/admin/"/>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
         <link rel="stylesheet" href="./assets/css/login.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
