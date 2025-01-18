b720bc302692282ce79302768f0bd6b5c641c11c
asurpbs <asur.1357268@gmail.com>
2025-01-18 11:21:45 +0530

    Revert "feat: Remove old bootstrap script and add new footer component with copyright notice and set to load metadata from /components/metadata.html and /components/metadata-admin.html"
    This reverts commit 8b695c27eefb49d1dd3804feaf23abf25095965b.

---
+++ data/meta-admin.php
@@ -0,0 +1,13 @@
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta name="title" content="Sola Chemical Company"/>
+    <meta name="description" content="Administrator site of the Sola Chemical Company"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="icon" type="image/png" href="/public/favicon-96x96.png" sizes="96x96"/>
+    <link rel="icon" type="image/svg+xml" href="/public/favicon.svg"/>
+    <link rel="shortcut icon" href="/public/favicon.ico"/>
+    <link rel="apple-touch-icon" sizes="180x180" href="/public/apple-touch-icon.png"/>
+    <link rel="manifest" href="./public/site.webmanifest"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+    <link rel="canonical" href="https://hashcoders.alwaysdata.net/admin/"/>
