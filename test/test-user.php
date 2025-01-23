d47df90147feae17043512e4bb089d9d51033140
asurpbs <asur.1357268@gmail.com>
2025-01-23 11:50:55 +0530

    refactor: clean up user creation code for better readability

--- test/test-user.php
+++ test/test-user.php
@@ -3,20 +3,20 @@ include "../classes/user.php";
 
 // Create a new user
 $newUser = new User(
-    null, // user_id is null for creating a new user
-    "John", // first_name
-    "Doe", // last_name
-    "john_doe.jpg", // image
-    "1", // gender
-    "1990-01-01", // birth_date
-    "password123", // password
-    "john.doe@example.com", // email
-    "123 Main St", // address1
-    "Apt 4B", // address2
-    "12345", // postal_code
-    1, // city_id
-    "1234567890", // telephone1
-    "0987654321" // telephone2
+    null,
+    "John",
+    "Doe",
+    "john_doe.jpg",
+    "1",
+    "1990-01-01",
+    "password123",
+    "john.doe@example.com",
+    "123 Main St",
+    "Apt 4B",
+    "12345",
+    1,
+    "1234567890",
+    "0987654321"
 );
 
 $newUser1 = new User(1);
