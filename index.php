4e5e446b811396603be611245b7706772762d1f9
asurpbs <asur.1357268@gmail.com>
2025-02-01 16:07:53 +0530

    fix: enhance input sanitization for page parameter

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
     <!-- Dynamic Content -->
     <div class="content">
       <?php
-      $page = filter_input(INPUT_GET, 'page', FILTER_SANITIZE_STRING) ?? 'home';
+      $page = filter_input(INPUT_GET, 'page', FILTER_SANITIZE_FULL_SPECIAL_CHARS) ?? 'home';
       $allowed_pages = ['home', 'about', 'product'];
       if (in_array($page, $allowed_pages)) {
         require_once "./pages/{$page}.php";
