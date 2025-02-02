fed04b6a212039398b1f13818beb464546c15b74
asurpbs <asur.1357268@gmail.com>
2025-02-02 21:43:01 +0530

    feat: enhance sign-up form with city selection and improved input handling

--- pages/signup.php
+++ pages/signup.php
@@ -1,5 +1,11 @@
 <?php
+require_once "../context/connect.php";
 require_once "../classes/user.php";
+
+// Fetch city data
+$stmt = $conn->prepare("SELECT id, name_en FROM city");
+$stmt->execute();
+$cities = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $user = new User(
@@ -26,6 +32,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <title>Sign in to Sola Chemicals</title>
     <!-- metadata -->
     <?php require_once '../components/metadata.html'; ?>
+    <!-- Include select2 CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="/assets/css/signup.css">
   </head>
 <body>
@@ -45,7 +53,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <!-- Last Name -->
             <div class="mb-3">
                 <label for="lastName" class="form-label">Enter Last Name</label>
-                <input name="last_name" type="text" class="form-control" id="lastName" placeholder="Last Name" value="<?php echo $_POST['last_name'] ?? ''; ?> required>
+                <input name="last_name" type="text" class="form-control" id="lastName" placeholder="Last Name" value="<?php echo $_POST['last_name'] ?? ''; ?>" required>
                 <span class="error-message" id="lastNameError"></span>
             </div>
 
@@ -53,7 +61,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <div class="mb-3">
                 <label for="gender" class="form-label">Select Gender</label>
                 <select class="form-select" id="gender" name="gender" required>
-                    <option value="<?php echo $_POST['gender'] ?? ''; ?>Select Gender</option>
+                    <option value=""><?php echo $_POST['gender'] ?? 'Select Gender'; ?></option>
                     <option value="m">Male</option>
                     <option value="f">Female</option>
                     <option value="o">Other</option>
@@ -64,14 +72,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <!-- Image Upload -->
             <div class="mb-3">
                 <label for="image" class="form-label">Upload Image</label>
-                <input type="file" class="form-control" id="image" accept="image/*" required>
+                <input name="image" type="file" class="form-control" id="image" accept="image/*" required>
                 <span class="error-message" name="image" id="imageError"></span>
             </div>
 
             <!-- Email -->
             <div class="mb-3">
                 <label for="email" class="form-label">Enter Email</label>
-                <input name="email" type="email" class="form-control" id="email" placeholder="Email" value="<?php echo $_POST['email'] ?? ''; ?> required>
+                <input name="email" type="email" class="form-control" id="email" placeholder="Email" value="<?php echo $_POST['email'] ?? ''; ?>" required>
                 <span class="error-message" id="emailError"></span>
             </div>
 
@@ -92,41 +100,46 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <!-- Address Line 1 -->
             <div class="mb-3">
                 <label for="address1" class="form-label">Address Line 1</label>
-                <input type="text" class="form-control" id="address1" placeholder="Address Line 1" required>
+                <input type="text" class="form-control" id="address1" placeholder="Address Line 1" value="<?php echo $_POST['address1'] ?? ''; ?>" required>
                 <span class="error-message" id="address1Error"></span>
             </div>
 
             <!-- Address Line 2 -->
             <div class="mb-3">
                 <label for="address2" class="form-label">Address Line 2</label>
-                <input type="text" class="form-control" id="address2" placeholder="Address Line 2">
+                <input type="text" class="form-control" id="address2" placeholder="Address Line 2" value="<?php echo $_POST['address2'] ?? ''; ?>">
             </div>
 
             <!-- Postal Code -->
             <div class="mb-3">
                 <label for="postalCode" class="form-label">Postal Code</label>
-                <input type="text" class="form-control" id="postalCode" placeholder="Postal Code" required>
+                <input type="text" class="form-control" id="postalCode" placeholder="Postal Code" value="<?php echo $_POST['postal_code'] ?? ''; ?>" required>
                 <span class="error-message" id="postalCodeError"></span>
             </div>
 
-            <!-- City -->
+            <!-- City Dropdown -->
             <div class="mb-3">
                 <label for="city" class="form-label">City</label>
-                <input type="text" class="form-control" id="city" placeholder="City" required>
+                <select class="form-control" id="city" name="city_id" required>
+                    <option value="">Select City</option>
+                    <?php foreach ($cities as $city): ?>
+                        <option value="<?php echo $city['id']; ?>"><?php echo $city['name_en']; ?></option>
+                    <?php endforeach; ?>
+                </select>
                 <span class="error-message" id="cityError"></span>
             </div>
 
             <!-- Telephone Number 1 -->
             <div class="mb-3">
                 <label for="telephoneNum1" class="form-label">Telephone Number 1</label>
-                <input type="tel" class="form-control" id="telephoneNum1" placeholder="Telephone Number 1" required>
+                <input type="tel" class="form-control" id="telephoneNum1" placeholder="Telephone Number 1" value="<?php echo $_POST['telephone1'] ?? ''; ?>" required>
                 <span class="error-message" id="telephoneNum1Error"></span>
             </div>
 
             <!-- Telephone Number 2 -->
             <div class="mb-3">
                 <label for="telephoneNum2" class="form-label">Telephone Number 2</label>
-                <input type="tel" class="form-control" id="telephoneNum2" placeholder="Telephone Number 2">
+                <input type="tel" class="form-control" id="telephoneNum2" placeholder="Telephone Number 2" value="<?php echo $_POST['telephone2'] ?? ''; ?>">
             </div>
 
             <!-- Submit Button -->
@@ -137,7 +150,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <p class="text-center mt-3">If you already have an account, <a href="/pages/signin.php">Sign in</a></p>
     </div>
 
-    <script src="/assets/bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js"></script>">
-    <script src="/assets/js/signup.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <script>
+    $(document).ready(function() {
+        $('#city').select2({
+            placeholder: 'Select a city',
+            allowClear: true
+        });
+    });
+    </script>
 </body>
 </html>
